6c2453a571c1c712d190681cd6d17fcad45163b8
baotuyloan1 <nguyenducbaokey@gmail.com>
2023-06-01 23:59:14 +0700

    add CorsMappings for frontend

--- src/main/java/com/bnd/ecommerce/config/MyConfig.java
+++ src/main/java/com/bnd/ecommerce/config/MyConfig.java
@@ -1,32 +1,35 @@
 package com.bnd.ecommerce.config;
 
-import com.bnd.ecommerce.service.ProductService;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.function.Supplier;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class MyConfig implements WebMvcConfigurer {
 
 
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    exposeDirectory("images/phone-photos", registry);
-    exposeDirectory("images/laptop-photos", registry);
-    exposeDirectory("images/tablet-photos", registry);
-  }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        exposeDirectory("images/phone-photos", registry);
+        exposeDirectory("images/laptop-photos", registry);
+        exposeDirectory("images/tablet-photos", registry);
+    }
 
-  private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
-    Path uploadDir = Paths.get(dirName);
-    String uploadPath = uploadDir.toFile().getAbsolutePath();
-    if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
-    registry
-        .addResourceHandler("/" + dirName + "/**")
-        .addResourceLocations("file:/" + uploadPath + "/");
-  }
+    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
+        Path uploadDir = Paths.get(dirName);
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
+        registry
+                .addResourceHandler("/" + dirName + "/**")
+                .addResourceLocations("file:/" + uploadPath + "/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*");
+    }
 }
